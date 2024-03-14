88ce52253a33f87b2efab826969b4f2c33a2770c
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-03-14 20:13:25 +0100

    delete line component scan in agorawebaplication

--- src/main/java/de/stella/agora_web/AgoraWebApplication.java
+++ src/main/java/de/stella/agora_web/AgoraWebApplication.java
@@ -2,7 +2,6 @@ package de.stella.agora_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
